a84c14aadf43de17229c6b839b2b832d53eb368e
Candidat-005 <g.thomas@laposte.net>
2024-03-25 09:19:04 +0100

    fini

--- src/app/api/contacts/route.tsx
+++ src/app/api/contacts/route.tsx
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import getContactsAll from "../getContacts";
 
 export async function GET() {
-    console.log("Contacts appelé !");
     const contacts = await getContactsAll();
     return NextResponse.json(contacts);
 }
