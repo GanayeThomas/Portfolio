02b11afdc976d9d1af49b9bac5669ebb0bf9476c
Candidat-005 <g.thomas@laposte.net>
2024-03-25 09:31:34 +0100

    fini

--- src/app/api/getContacts.ts
+++ src/app/api/getContacts.ts
@@ -3,9 +3,11 @@ import { Contact } from "@/components/Contacts";
 
 export default async function getContactsAll(): Promise<Contact[]> {
     const contacts = await prisma.contact.findMany();
+    // Mappage de chaque contact récupéré pour inclure la propriété `type`,
+    // en utilisant la fonction `determineContactType` pour déterminer cette propriété.
     return contacts.map(contact => ({
-        ...contact,
-        type: determineContactType(contact),
+        ...contact, // Utilisation de l'opérateur de décomposition pour inclure toutes les propriétés existantes du contact.
+        type: determineContactType(contact), // Ajout ou écrasement de la propriété `type` avec le résultat de `determineContactType`.
     }));
 }
 
